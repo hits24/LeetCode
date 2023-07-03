3d91d796a37817baae15d44cad66b4fcfd9c10ea
Hitashri Patil <101586626+hits24@users.noreply.github.com>
2023-07-03 22:13:58 +0530

    Time: 4 ms (94.64%), Space: 43.8 MB (51.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,30 +2,28 @@ class Solution {
     public int romanToInt(String s) {
         int ans=0, num=0;
         for(int i=s.length()-1; i>=0; i--){
-            switch(s.charAt(i)){
-                case 'I' : num=1;
+            switch (s.charAt(i)){
+                case 'I': num=1;
                 break;
-                case 'V' : num=5;
+                case 'V': num=5;
                 break;
-                case 'X' : num=10;
+                case 'X': num=10;
                 break;
-                case 'L' : num=50;
+                case 'L': num=50;
                 break;
-                case 'C' : num=100;
+                case 'C': num=100;
                 break;
-                case 'D' : num=500;
+                case 'D': num=500;
                 break;
-                case 'M' : num=1000;
+                case 'M': num=1000;
                 break;
-                
 
             }
-            
             if(4*num < ans){
-                ans -= num;
+                ans-=num;
             }
             else{
-                ans += num;
+                ans+=num;
             }
         }
         return ans;
